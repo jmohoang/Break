d8d4d6b878b5ccef06d4fe031a42180971ae4bda
Libby Shoop <shoop@macalester.edu>
2014-10-10 11:51:53 -0500

    structure for the hw5 breakout program

---
+++ src/edu/macalester/comp124/breakout/BreakoutProgram.java
@@ -0,0 +1,12 @@
+package edu.macalester.comp124.breakout;
+
+import acm.program.GraphicsProgram;
+
+/**
+ * Main GraphicsProgram for the breakout game described
+ * in exercise 10.10 int the Roberts Textbook.
+ *
+ */
+public class BreakoutProgram extends GraphicsProgram {
+
+}
