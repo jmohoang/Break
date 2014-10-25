dcbe68a06a2c70e07fceb0c307de37843b26a2b7
John <jmohoang@macalester.edu>
2014-10-24 23:35:06 -0500

    Fixed todos

---
+++ src/edu/macalester/comp124/breakout/Brick.java
@@ -0,0 +1,23 @@
+package edu.macalester.comp124.breakout;
+
+import acm.graphics.GCompound;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+
+import java.awt.*;
+
+/**
+ * Created by John on 20-Oct-14.
+ */
+public class Brick extends GCompound {
+    private GRect brick;
+    public static final int BRICK_HEIGHT = 20;
+    public static final int BRICK_WIDTH = 60;
+
+    public Brick (Color f){
+        brick = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
+        brick.setFilled(true);
+        brick.setFillColor(f);
+        add(brick);
+    }
+}
