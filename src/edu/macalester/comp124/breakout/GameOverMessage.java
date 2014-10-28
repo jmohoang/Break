4c5e034d412ae5c2a80976fb456d3c8b04b1f105
John <jmohoang@macalester.edu>
2014-10-28 13:29:52 -0500

    Fixed todos

--- src/edu/macalester/comp124/breakout/GameOverMessage.java
+++ src/edu/macalester/comp124/breakout/GameOverMessage.java
@@ -7,10 +7,10 @@ import java.awt.*;
 
 /**
  * Created by John on 27-Oct-14.
+ * Creates a GCompound with a GLabel in it, to write ou the Game over message
  */
 public class GameOverMessage extends GCompound {
     private GLabel message;
-
 
     public GameOverMessage (){
         message = new GLabel("GAME OVER");
