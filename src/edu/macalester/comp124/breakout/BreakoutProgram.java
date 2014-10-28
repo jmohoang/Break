ae2effa279f0b7d660e7984f2a1dd6b2b10139b4
John <jmohoang@macalester.edu>
2014-10-28 13:30:28 -0500

    Fixed todos

--- src/edu/macalester/comp124/breakout/BreakoutProgram.java
+++ src/edu/macalester/comp124/breakout/BreakoutProgram.java
@@ -9,7 +9,7 @@ import java.awt.event.MouseEvent;
 /**
  * Main GraphicsProgram for the breakout game described
  * in exercise 10.10 int the Roberts Textbook.
- *
+ *Created by John Mohoang
  */
 public class BreakoutProgram extends GraphicsProgram {
     Paddle paddle1;
@@ -26,22 +26,38 @@ public class BreakoutProgram extends GraphicsProgram {
     int totalBrickNum; //Keeps track of the total number of bricks.
     RandomGenerator rgen;
 
-
+    /**
+     * Constructor for the breakout game.
+     * I used it mainly to construct the tries which holds the number of times the run method has been called
+     */
     public BreakoutProgram (){
         tries = 1;
         rgen = new RandomGenerator();
     }
 
-
+    /**
+     * The init method was used to add the mouse listener.
+     */
     public void init (){
         addMouseListeners();
     }
+
+    /**
+     * The method that describes the mouseMoved event.
+     * The mouseMoved is used to move the paddle such that it follows the x coordinate of the mouse pointer
+     * @param e
+     */
     public void mouseMoved (MouseEvent e){
         lastX = e.getX();
         paddle1.move(lastX - paddle1.getX(), 0);
         pause (5);
 
     }
+
+    /**
+     * Main run function of the breakout game
+     * When this run function is called, the game will play.
+     */
     public void run() {
         totalBrickNum = 100;
         playOn = true;
@@ -60,10 +76,15 @@ public class BreakoutProgram extends GraphicsProgram {
         add(message, 200, 400);
     }
 
+    /**
+     * Takes in a GObject which in the case of the breakout game is supposed to be the ball.
+     * It drops off the ball towards the paddle at a random angle
+     * and it also keeps the ball moving until either the ball misses the paddle or the bricks are all hit
+     * @param obj
+     */
     public void moveBall (GCompound obj){
-        dX = rgen.nextDouble(0, 5);
-        dY = 2; //rgen.nextDouble(0.1, 2);
-        if (rgen.nextBoolean(0.5)){ dX = -dX;}
+        dX = rgen.nextDouble(-2, 2);
+        dY = rgen.nextDouble(0.1, 2);
 
         while (playOn){
             obj.move(dX, dY);
@@ -87,15 +108,18 @@ public class BreakoutProgram extends GraphicsProgram {
             getElementAt(obj.getX(), obj.getY()+Ball.BALL_DIAMETER) == paddle1 ||
             getElementAt(obj.getX()+Ball.BALL_DIAMETER, obj.getY()+Ball.BALL_DIAMETER) == paddle1)
             {dY = -dY;}
-
-//            if (getElementAt(obj.getX()+Ball.BALL_DIAMETER, obj.getY()) == paddle1) {dY = -dY;}
-//            if (getElementAt(obj.getX()+Ball.BALL_DIAMETER, obj.getY()+Ball.BALL_DIAMETER) == paddle1){dY = -dY;}
             //deleting the bricks
             deleteBricks();
             previousX = ball1.getX();
             previousY = ball1.getY();
         }
     }
+
+    /**
+     * This method is used to make the bricks disappear when the are hit by the bal.
+     * All the four corners of the ball square are defined
+     * as well each of the sides of a brick that each corner of the ball square is likely to come in contact with
+     */
     public void deleteBricks (){
         double x = ball1.getX();
         double y = ball1.getY(); //coordinates of the top left corner of the ball
