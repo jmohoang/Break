7d06d2e8104bc838c7de84ba951358f6b979ef67
John <jmohoang@macalester.edu>
2014-10-28 13:30:10 -0500

    Fixed todos

--- src/edu/macalester/comp124/breakout/Brick.java
+++ src/edu/macalester/comp124/breakout/Brick.java
@@ -8,6 +8,7 @@ import java.awt.*;
 
 /**
  * Created by John on 20-Oct-14.
+ * Creates a GCompound that encompasses an individual brick in order to make the wall
  */
 public class Brick extends GCompound {
     private GRect brick;
